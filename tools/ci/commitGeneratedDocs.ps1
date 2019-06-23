d46aa9fbe1f98e3e9b99c309f0aea073816d6134
Jerome Humbert <jerome.humbert@microsoft.com>
2019-06-23 22:18:00 +0100

    Ensure working with git inside sub-repo

--- tools/ci/commitGeneratedDocs.ps1
+++ tools/ci/commitGeneratedDocs.ps1
@@ -37,20 +37,26 @@ if (-not $output)
     exit 1
 }
 
+# Get info about last change associated with the new generated docs
+Write-Host "Get the SHA1 and title of the most recent commit"
+$commitSha = git log -1 --pretty=%H
+$commitTitle = git log -1 --pretty=%s
+Write-Host "${commitSha}: $commitTitle"
+
 # Clean the _docs/ folder to avoid any interaction with a previous build if the agent is not clean
 Write-Host "Clean output folder '_docs/' if it exists"
 Remove-Item ".\_docs" -Force -Recurse -ErrorAction Ignore
 
-# Create or clone destination branch
+# Create or clone destination branch.
+# Note that this creates a second copy of the repository inside itself.
+# This will be used to only commit changes to that docs/* branch which
+# contains only generated documentation-related files, and not the code.
 $DestBranch = "docs/$SourceBranch"
 $output = ""
 Invoke-Expression "git rev-parse --verify `"refs/remotes/origin/$DestBranch^{commit}`"" | Tee-Object -Variable output | Write-Host
 if ($output)
 {
     # Clone the destination branch locally in a temporary folder.
-    # Note that this creates a second copy of the repository inside itself.
-    # This is fine as long as we stay out of the _docs directory to keep using
-    # the outer repository and its config we set above.
     Write-Host "Clone the generated docs branch"
     git clone https://github.com/Microsoft/MixedReality-WebRTC.git --branch $DestBranch ".\_docs"
     
@@ -58,47 +64,44 @@ if ($output)
     # of the documentation are effectively deleted in the commit.
     Write-Host "Delete currently committed version"
     Get-ChildItem ".\_docs" -Recurse | Remove-Item -Force -Recurse
+    
+    # Move inside sub-repository
+    Set-Location ".\_docs"
 }
 else
 {
     # Create the destination branch and checkout locally in a temporary folder.
-    # Note that the orphan branch is created inside the main repository,
-    # and unlike the case above there's no inner repository created.
     Write-Host "Creating new destination branch $DestBranch"
     New-Item ".\_docs" -ItemType Directory
+    Set-Location ".\_docs"
+    git init
+    git remote add origin https://github.com/microsoft/MixedReality-WebRTC
     git checkout --orphan "$DestBranch"
 }
 
 # Because the _docs folder is now empty, always re-generate the README.md
 # This could be skipped if existing, but allows upgrading to a newer template if needed
 Write-Host "Generate README.md:"
-(Get-Content -Path docs/README.template.md -Encoding UTF8) -Replace '\$branchname',"$SourceBranch" | Set-Content -Path _docs/README.md -Encoding UTF8
+(Get-Content -Path ../docs/README.template.md -Encoding UTF8) -Replace '\$branchname',"$SourceBranch" | Set-Content -Path README.md -Encoding UTF8
 Write-Host "--------------------"
-Get-Content -Path _docs/README.md -Encoding UTF8 | Write-Host
+Get-Content -Path README.md -Encoding UTF8 | Write-Host
 Write-Host "--------------------"
 Write-Host "Commit README.md"
-git add _docs/README.md
+git add README.md
 git commit -m "Add README.md for generated branch $DestBranch"
 git log -1 --format=full
 
-# Get info about last change associated with the new generated docs
-Write-Host "Get the SHA1 and title of the most recent commit"
-$commitSha = git log -1 --pretty=%H
-$commitTitle = git log -1 --pretty=%b
-Write-Host "${commitSha}: $commitTitle"
-
 # Copy the newly-generated version of the docs
 Write-Host "Copy new generated version"
-Copy-Item ".\build\docs\generated\*" -Destination ".\_docs" -Force -Recurse
+Copy-Item "..\build\docs\generated\*" -Destination ".\" -Force -Recurse
 
 # Check for any change compared to previous version (if any)
 Write-Host "Check for changes"
-Set-Location ".\_docs"
 if (git status --short)
 {
   git add --all
   git commit -m "Docs for commit $commitSha ($commitTitle)"
-  git push origin $DestBranch
+  #git push origin $DestBranch
   Write-Host "Docs changes committed"
 }
 else
