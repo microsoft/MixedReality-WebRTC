b100afff2eb5d005d2c6c27911674ce465b86ffc
Jerome Humbert <jerome.humbert@microsoft.com>
2019-07-08 15:59:43 +0100

    Allow toggling local video with MRTK button

---
+++ libs/Microsoft.MixedReality.WebRTC.Unity/Assets/Microsoft.MixedReality.WebRTC.Unity.Examples/MrtkVideoChatDemo/ToggleEnabled.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
+
+/// <summary>
+/// Simple helper component to toggle the enabled state of another component.
+/// </summary>
+public class ToggleEnabled : MonoBehaviour
+{
+    public MonoBehaviour ToggleTarget;
+
+    public void Toggle()
+    {
+        ToggleTarget.enabled = !ToggleTarget.enabled;
+    }
+}
