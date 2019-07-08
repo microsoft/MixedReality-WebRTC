aee296043c4a1d7065d554e1d0176a9550a4beee
Jerome Humbert <jerome.humbert@microsoft.com>
2019-07-08 10:43:33 +0100

    Add data channel SCTP failsafe test

---
+++ libs/Microsoft.MixedReality.WebRTC.Native/test/data_channel_tests.cpp
@@ -0,0 +1,20 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license
+// information.
+
+#include "pch.h"
+
+#include "../include/api.h"
+
+#if !defined(MRSW_EXCLUDE_DEVICE_TESTS)
+
+// Fail to add channel before the PeerConnection is initialized
+TEST(DataChannel, AddChannelBeforeInit) {
+  PCRaii pc;
+  ASSERT_NE(nullptr, pc.handle());
+  ASSERT_EQ(false, mrsPeerConnectionAddDataChannel(
+                       pc.handle(), -1, "data", true, true, nullptr, nullptr,
+                       nullptr, nullptr, nullptr, nullptr));
+}
+
+#endif  // MRSW_EXCLUDE_DEVICE_TESTS
