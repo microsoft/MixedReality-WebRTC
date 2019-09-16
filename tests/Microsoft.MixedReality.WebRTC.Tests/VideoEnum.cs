9a81c94cf01786398495f8046b645b7b28d987de
Jerome Humbert <jerome.humbert@microsoft.com>
2019-09-16 17:24:16 +0900

    Add C# test for video device/format enumeration
    Add a C# unit test checking that `GetVideoCaptureDevicesAsync()` and
    `GetVideoCaptureFormatsAsync()` work as intended, at least if the host
    device where the test runs has a video capture device attached.
    
    Note that `GetVideoCaptureFormatsAsync()` is not yet available. It is
    coming in a future change.

---
+++ tests/Microsoft.MixedReality.WebRTC.Tests/VideoEnum.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.MixedReality.WebRTC;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace Microsoft.MixedReality.WebRTC.Tests
+{
+    /// <summary>
+    /// Test the static methods to enumerate the video capture devices and their capture formats.
+    /// </summary>
+    [TestFixture]
+    internal class VideoEnum
+    {
+        /// <summary>
+        /// Check that GetVideoCaptureDevicesAsync() successfully returns, and that if there is
+        /// any device then its ID and name are not empty.
+        /// </summary>
+        [Test]
+        public void EnumVideoDevices()
+        {
+            PeerConnection.GetVideoCaptureDevicesAsync().ContinueWith((enumTask) =>
+            {
+                Assert.IsNull(enumTask.Exception);
+                List<PeerConnection.VideoCaptureDevice> devices = enumTask.Result;
+                foreach (var device in devices)
+                {
+                    Assert.That(device.id.Length, Is.GreaterThan(0));
+                    Assert.That(device.name.Length, Is.GreaterThan(0));
+                }
+            });
+        }
+
+        /// <summary>
+        /// Check that, for all available video capture devices on the host device,
+        /// GetVideoCaptureFormatsAsync() returns some valid formats.
+        /// </summary>
+        //[Test]
+        //public void EnumVideoFormats()
+        //{
+        //    PeerConnection.GetVideoCaptureDevicesAsync().ContinueWith((enumDeviceTask) =>
+        //    {
+        //        Assert.IsNull(enumDeviceTask.Exception);
+        //        List<PeerConnection.VideoCaptureDevice> devices = enumDeviceTask.Result;
+        //        if (devices.Count == 0)
+        //        {
+        //            Assert.Inconclusive("Host device has no available video capture device.");
+        //        }
+
+        //        foreach (var device in devices)
+        //        {
+        //            PeerConnection.GetVideoCaptureFormatsAsync(device.id).ContinueWith((enumFormatTask) =>
+        //            {
+        //                Assert.IsNull(enumFormatTask.Exception);
+        //                // TODO...
+        //            }
+        //        }
+        //    });
+        //}
+    }
+}
