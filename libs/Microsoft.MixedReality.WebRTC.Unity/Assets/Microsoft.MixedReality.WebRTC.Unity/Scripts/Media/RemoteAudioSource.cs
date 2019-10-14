3fa8108272a2d6cae9365c5f402b81f5b9de4c8b
Stephen Kennedy <stephen.kennedy@microsoft.com>
2019-10-14 16:26:08 +0100

    Remote audio feature

--- libs/Microsoft.MixedReality.WebRTC.Unity/Assets/Microsoft.MixedReality.WebRTC.Unity/Scripts/Media/RemoteAudioSource.cs
+++ libs/Microsoft.MixedReality.WebRTC.Unity/Assets/Microsoft.MixedReality.WebRTC.Unity/Scripts/Media/RemoteAudioSource.cs
@@ -3,8 +3,8 @@
 
 using System;
 using System.Collections.Concurrent;
+using System.Threading;
 using UnityEngine;
-using UnityEngine.Events;
 
 namespace Microsoft.MixedReality.WebRTC.Unity
 {
@@ -41,6 +41,163 @@ namespace Microsoft.MixedReality.WebRTC.Unity
         private ConcurrentQueue<Action> _mainThreadWorkQueue = new ConcurrentQueue<Action>();
 
         /// <summary>
+        /// Clip corresponding to the remote audio track.
+        /// </summary>
+        private AudioClip _audioClip; // created when the first webrtc frame arrives in RemoteAudioFrameReady.
+
+        /// <summary>
+        /// Set when we've enqueued the job to create the audio clip on the main thread. (to avoid duplication)
+        /// </summary>
+        private volatile bool _audioClipEnqueuedCreate = false;
+
+        /// <summary>
+        /// Internal adaptor from webrtc frame data to unity audio clip.
+        /// </summary>
+        class AudioData
+        {
+            float[] _buffer = null;
+            bool _isFull = false;
+            int _readPos = 0; // AudioClip fetches start from here
+            int _writePos = 0; // WebRTC data gets appended here
+            int _channelCount = 0;
+            int _sampleRate = 0;
+            public Action<int, int> AudioFormatChanged = null;
+
+            void UpdateInternalFormat(AudioFrame frame)
+            {
+                if (frame.sampleRate != _sampleRate
+                    || frame.channelCount != _channelCount
+                    || _buffer == null
+                    || _buffer.Length != (frame.sampleRate * frame.channelCount))
+                {
+                    // If anything changes, let's drop the buffer for simplicity (i.e. we won't resample or do extra work)
+                    _sampleRate = (int)frame.sampleRate;
+                    _channelCount = (int)frame.channelCount;
+                    _buffer = new float[frame.sampleRate * frame.channelCount];
+                    _readPos = 0;
+                    _writePos = 0;
+                    AudioFormatChanged?.Invoke(_sampleRate, _channelCount);
+                }
+            }
+
+            internal void AppendAudio16(AudioFrame frame)
+            {
+                Debug.Assert(Monitor.IsEntered(this));
+                UpdateInternalFormat(frame);
+                Debug.Assert((frame.frameCount * frame.channelCount) <= _buffer.Length);
+                unsafe
+                {
+                    short* src = (short*)frame.audioData;
+                    int spaceAtEnd = _buffer.Length - _writePos;
+                    int srcSamples = (int)(frame.frameCount * frame.channelCount);
+                    // write1 is from _writePos forwards. write2 is the wraparound part (very often 0)
+                    int write1 = Math.Min(spaceAtEnd, srcSamples);
+                    for (int i = 0; i < write1; ++i)
+                    {
+                        _buffer[i + _writePos] = src[i] / 32768.0f; // 16 bit data is signed. Remap to -1,1
+                    }
+
+                    // Adjust the _writePos. Note that this may also update *_readPos*, if the reader is slow.
+                    // We will always enque ALL the data on the basis that newer is better.
+                    // Sometimes, if the reader isn't consuming fast enough, we may advance 'through' the read position.
+                    // This will give bad artifacts, so instead we'll drop some of the input instead.
+
+                    if (write1 == srcSamples)
+                    {
+                        var newPos = _writePos + write1;
+                        if (_isFull || (_writePos < _readPos && _readPos < newPos))
+                        {
+                            _readPos = newPos;
+                            _isFull = true;
+                        }
+                        _writePos = newPos;
+                    }
+                    else
+                    {
+                        Debug.Assert(_writePos == _buffer.Length);
+                        int write2 = srcSamples - write1;
+                        for (int i = 0; i < write2; ++i)
+                        {
+                            _buffer[i] = src[write1 + i] / 32768.0f;
+                        }
+                        // Notation - *=sample, W=writePos, R=readPos, B=both, .=empty
+                        // Case Full or [**W...R**] or [..R***W..] => [***B****]
+                        if (_isFull || (_writePos < _readPos) || (_readPos < write2))
+                        {
+                            _readPos = write2;
+                            _isFull = true;
+                        }
+                        _writePos = write2;
+                    }
+                }
+            }
+
+            internal void OnClipRead(float[] data)
+            {
+                lock (this)
+                {
+                    Debug.Assert(data.Length > 0);
+                    Debug.Assert(!_isFull || (_readPos == _writePos));
+                    if (_buffer == null || (_readPos==_writePos && !_isFull))
+                    {
+                        return; // null or empty buffer
+                    }
+                    _isFull = false; // We are going to consume some
+
+                    int totalRead;
+                    if (_readPos < _writePos) // simple case - only a single copy
+                    {
+                        int avail1 = _writePos - _readPos;
+                        int read1 = Math.Min(avail1, data.Length);
+                        for (int i = 0; i < read1; ++i)
+                        {
+                            data[i] = _buffer[_readPos + i];
+                        }
+                        totalRead = read1;
+                        _readPos += read1;
+                    }
+                    else // ( _readPos >= _writePos ) // [***W....R***] or [***B****], potential split read
+                    {
+                        int read1 = _buffer.Length - _readPos;
+                        if ( data.Length <= read1) // single read
+                        {
+                            for (int i = 0; i < data.Length; ++i)
+                            {
+                                data[i] = _buffer[_readPos + i];
+                            }
+                            totalRead = data.Length;
+                            _readPos += data.Length;
+                        }
+                        else // split read
+                        {
+                            for (int i = 0; i < read1; ++i)
+                            {
+                                data[i] = _buffer[_readPos + i];
+                            }
+                            int read2 = Math.Min(data.Length - read1, _writePos);
+                            for (int i = 0; i < read2; ++i)
+                            {
+                                data[i + read1] = _buffer[i];
+                            }
+                            totalRead = read1 + read2;
+                            _readPos = read2;
+                        }
+                    }
+                    // zero fill if we underflowed
+                    for (int i = totalRead; i < data.Length; ++i)
+                    {
+                        data[i] = 0;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Local storage of audio data to be fed to _audioClip;
+        /// </summary>
+        private AudioData _audioData = new AudioData();
+
+        /// <summary>
         /// Manually start playback of the remote audio feed by registering some listeners
         /// to the peer connection and starting to enqueue audio frames as they become ready.
         /// 
@@ -57,8 +214,9 @@ namespace Microsoft.MixedReality.WebRTC.Unity
         {
             if (!IsPlaying)
             {
+                _audioData.AudioFormatChanged = OnAudioFormatChanged;
                 IsPlaying = true;
-                //PeerConnection.Peer.RemoteAudioFrameReady += RemoteAudioFrameReady;
+                PeerConnection.Peer.RemoteAudioFrameReady += RemoteAudioFrameReady;
             }
         }
 
@@ -77,7 +235,8 @@ namespace Microsoft.MixedReality.WebRTC.Unity
             if (IsPlaying)
             {
                 IsPlaying = false;
-                //PeerConnection.Peer.RemoteAudioFrameReady -= RemoteAudioFrameReady;
+                PeerConnection.Peer.RemoteAudioFrameReady -= RemoteAudioFrameReady;
+                _audioClip = null;
             }
         }
 
@@ -173,9 +332,73 @@ namespace Microsoft.MixedReality.WebRTC.Unity
             }
         }
 
-        //private void RemoteAudioFrameReady(AudioFrame frame)
-        //{
-        //    FrameQueue.Enqueue(frame);
-        //}
+        private void EnqueueCreateAudioClipOnMainThread(int sampleRate, int channelCount)
+        {
+            Debug.Assert(_audioClipEnqueuedCreate == false);
+            _audioClipEnqueuedCreate = true;
+            _mainThreadWorkQueue.Enqueue(() =>
+            {
+                int lengthSamples = sampleRate / 10; // Buffer up to 100ms of data.
+                _audioClip = AudioClip.Create("RemoteClip", sampleRate, channelCount, sampleRate, true, _audioData.OnClipRead);
+                _audioClipEnqueuedCreate = false;
+                var aud = GetComponent<UnityEngine.AudioSource>();
+                if (aud)
+                {
+                    aud.clip = _audioClip;
+                    aud.volume = 1.0f;
+                    aud.loop = true;
+                    aud.bypassEffects = true;
+                    aud.Play();
+                }
+            });
+        }
+
+        private void OnAudioFormatChanged(int sampleRate, int channelCount)
+        {
+            lock (this)
+            {
+                _audioClip = null;
+                // TODO Race condition here. AudioSource.clip still refers to the old clip. We can't modify it (not main thread).
+                // Thus we may get some calls to _audioData.OnClipRead (which is expecting old samplerate/channelcount)
+                // before the new clip is created.
+                EnqueueCreateAudioClipOnMainThread(sampleRate, channelCount);
+            }
+        }
+
+        private void RemoteAudioFrameReady(AudioFrame frame)
+        {
+            // The two parts of getting WebRTC audio:
+            //
+            // _audioData buffers the audioframes which arrive via WebRTC callbacks.
+            // WebRTC will sometimes change the format midstream, and if so, we update to match the new
+            // format immediately and will need to create a new AudioClip instance with the new parameters.
+            // Note we can't change AudioClip immediately since this is not the unity main thread.
+            //
+            // _audioClip is the unity object which can be attached to various audio emitters.
+            // The audioclip in this class is created with callbacks which read the data from _audioBuffer.
+            if (_audioClipEnqueuedCreate)
+            {
+                return; // waiting for creation.
+            }
+            if (_audioClip == null)
+            {
+                // Don't enqueue any audio data until we have a clip
+                EnqueueCreateAudioClipOnMainThread((int)frame.sampleRate, (int)frame.channelCount);
+                return;
+            }
+            lock (_audioData)
+            {
+                switch (frame.bitsPerSample)
+                {
+                    case 16:
+                        _audioData.AppendAudio16(frame);
+                        break;
+                    case 8:
+                    default:
+                        Debug.LogError($"Unsupported audio format {frame.bitsPerSample} bits per sample");
+                        break;
+                }
+            }
+        }
     }
 }
