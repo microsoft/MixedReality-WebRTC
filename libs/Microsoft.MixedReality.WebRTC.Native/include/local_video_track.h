5498d1d06eae2a7f7ce6015f486073fe1de00ecf
Jerome Humbert <jerome.humbert@microsoft.com>
2019-11-21 15:39:53 +0000

    Add documenting comments to local_video_track.h

--- libs/Microsoft.MixedReality.WebRTC.Native/include/local_video_track.h
+++ libs/Microsoft.MixedReality.WebRTC.Native/include/local_video_track.h
@@ -16,6 +16,18 @@ namespace Microsoft::MixedReality::WebRTC {
 
 class PeerConnection;
 
+/// A local video track is a media track for a peer connection backed by a local
+/// source, and transmitted to a remote peer.
+///
+/// The local nature of the track implies that the local peer has control on it,
+/// including enabling or disabling the track, and removing it from the peer
+/// connection. This is in contrast with a remote track	reflecting a track sent
+/// by the remote peer, for which the local peer has limited control.
+///
+/// The local video track is backed by a local video track source. This is
+/// typically a video capture device (e.g. webcam), but can	also be a source
+/// producing programmatically generated frames. The local video track itself
+/// has no knowledge about how the source produces the frames.
 class LocalVideoTrack : public VideoFrameObserver,
                         public rtc::RefCountInterface {
  public:
@@ -25,8 +37,14 @@ class LocalVideoTrack : public VideoFrameObserver,
                   mrsLocalVideoTrackInteropHandle interop_handle) noexcept;
   MRS_API ~LocalVideoTrack() override;
 
-  MRS_API [[nodiscard]] bool IsEnabled() const noexcept;
+  /// Enable or disable the video track. An enabled track streams its content
+  /// from its source to the remote peer. A disabled video track only sends
+  /// black frames.
   MRS_API void SetEnabled(bool enabled) const noexcept;
+
+  /// Check if the track is enabled.
+  /// See |SetEnabled(bool)|.
+  MRS_API [[nodiscard]] bool IsEnabled() const noexcept;
 
   //
   // Advanced use
@@ -48,7 +66,7 @@ class LocalVideoTrack : public VideoFrameObserver,
   void RemoveFromPeerConnection(webrtc::PeerConnectionInterface& peer);
 
  private:
-  /// PeerConnection object owning this track.
+  /// Weak reference to the PeerConnection object owning this track.
   PeerConnection* owner_{};
 
   /// Underlying core implementation.
