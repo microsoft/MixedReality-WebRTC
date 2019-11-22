127a3feaaa4c50fc7be141e312d853fa9397df2c
Jerome Humbert <jerome.humbert@microsoft.com>
2019-11-22 15:58:14 +0000

    Add some comments on PeerConnectionHandle

--- libs/Microsoft.MixedReality.WebRTC/Interop/PeerConnectionInterop.cs
+++ libs/Microsoft.MixedReality.WebRTC/Interop/PeerConnectionInterop.cs
@@ -6,13 +6,27 @@ using System.Runtime.InteropServices;
 
 namespace Microsoft.MixedReality.WebRTC.Interop
 {
+    /// <summary>
+    /// Handle to a native peer connection object.
+    /// </summary>
     public sealed class PeerConnectionHandle : SafeHandle
     {
+        /// <summary>
+        /// Make a copy of the current handle.
+        /// </summary>
+        /// <returns>The newly created handle.</returns>
         public PeerConnectionHandle MakeCopy()
         {
             return new PeerConnectionHandle(handle);
         }
 
+        /// <summary>
+        /// Check if the current handle is invalid, which means it is not referencing
+        /// an actual native object. Note that a valid handle only means that the internal
+        /// handle references a native object, but does not guarantee that the native
+        /// object is still accessible. It is only safe to access the native object if
+        /// the handle is not closed, which implies it being valid.
+        /// </summary>
         public override bool IsInvalid
         {
             get
@@ -21,15 +35,26 @@ namespace Microsoft.MixedReality.WebRTC.Interop
             }
         }
 
+        /// <summary>
+        /// Default constructor for an invalid handle.
+        /// </summary>
         public PeerConnectionHandle() : base(IntPtr.Zero, ownsHandle: true)
         {
         }
 
+        /// <summary>
+        /// Constructor for a valid handle referencing the given native object.
+        /// </summary>
+        /// <param name="handle">The valid internal handle to the native object.</param>
         public PeerConnectionHandle(IntPtr handle) : base(IntPtr.Zero, ownsHandle: true)
         {
             SetHandle(handle);
         }
 
+        /// <summary>
+        /// Release the native object while the handle is being closed.
+        /// </summary>
+        /// <returns>Return <c>true</c> if the native object was successfully released.</returns>
         protected override bool ReleaseHandle()
         {
             PeerConnectionInterop.PeerConnection_RemoveRef(handle);
