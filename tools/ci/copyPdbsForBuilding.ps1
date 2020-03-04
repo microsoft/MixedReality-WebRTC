d95a697a56ed1797c302568f161a45cc0aa66350
Jerome Humbert <jerome.humbert@microsoft.com>
2020-03-04 10:35:44 +0000

    Move PDBs to bin/ for linker to find them

--- tools/ci/copyPdbsForBuilding.ps1
+++ tools/ci/copyPdbsForBuilding.ps1
@@ -1,42 +1,20 @@
-# Copy all PDBs from their packaging folder back to their original location
+# Copy all PDBs from their packaging folder back next to webrtc.lib
+# Note that this is not their original location, but this is where
+# the linker currently looks for them.
 
 param(
     [string]$SourcePath,
-    [string]$CorePath,
-    [string]$WrapperPath,
-    [string]$WrapperGluePath,
-    [ValidateSet('Debug','Release')]
-    [string]$BuildConfig,
-    [ValidateSet('Win32','UWP')]
-    [string]$BuildPlatform
+    [string]$OutputPath
 )
 
-# List content of source folder
-Write-Host "Content of source folder $SourcePath"
-foreach ($f in $(Get-ChildItem -Path $SourcePath -Recurse))
+# Move all PDBs
+Write-Host "Moving PDBs..."
+mkdir -Force $OutputPath | out-null
+Move-Item -Path $(Join-Path $SourcePath "*") -Destination $OutputPath -Include "*.pdb"
+
+# List content of output folder
+Write-Host "Content of output folder $OutputPath"
+foreach ($f in $(Get-ChildItem -Path $OutputPath -Recurse))
 {
     Write-Host $f.FullName
 }
-
-if ($BuildPlatform -eq "UWP")
-{
-    # Move wrapper PDB (Org.WebRtc.pdb)
-    Write-Host "Moving PDB for Org.WebRtc.dll..."
-    mkdir -Force $WrapperPath | out-null
-    Move-Item -Path $(Join-Path $SourcePath "Org.WebRtc.pdb") -Destination $WrapperPath
-
-    # Move wrapper glue PDB (Org.WebRtc.WrapperGlue.pdb)
-    # In Release there is no PDB; the project does not specify /DEBUG so defaults to Debug-only PDBs.
-    if ($BuildConfig -eq "Release") {
-        Write-Host "Skipping PDB for Org.WebRtc.WrapperGlue.lib (not generated in Release)"
-    } else {
-        Write-Host "Moving PDB for Org.WebRtc.WrapperGlue.lib..."
-        mkdir -Force $WrapperGluePath | out-null
-        Move-Item -Path $(Join-Path $SourcePath "Org.WebRtc.WrapperGlue.pdb") -Destination $WrapperGluePath
-    }
-}
-
-# Copy core PDBs
-Write-Host "Moving PDBs for core webrtc.lib..."
-mkdir -Force $CorePath | out-null
-Move-Item -Path $(Join-Path $SourcePath "*") -Destination $CorePath -Include "*.pdb"
