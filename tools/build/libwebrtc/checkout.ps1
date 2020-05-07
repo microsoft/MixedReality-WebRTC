ed84a01daf1fa1cce5a700436404621f38c1f4fa
Jerome Humbert <humbert.jerome@outlook.com>
2020-05-07 17:44:32 +0100

    Split checkout and build PS scripts
    Split the Powershell checkout and build scripts for Windows, and
    properly handle the various build variants with a triple of arguments.
    
    Note that this change disables ARM(32), which is not supported on this
    branch due to MSVC ARM32 support being unavailable in Google's scripts
    (see build/vs_toolchain.py:CopyDlls).

---
+++ tools/build/libwebrtc/checkout.ps1
@@ -0,0 +1,17 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+# Run in scoped block to not affect user's $env:PATH
+& powershell {
+    # Import library
+    . .\mrwebrtc.ps1
+
+    # Setup the build environment
+    Initialize-BuildEnvironment
+
+    # Download and setup the Google repository
+    Install-GoogleRepository
+
+    # Install additional dependencies
+    Install-Dependencies
+}
