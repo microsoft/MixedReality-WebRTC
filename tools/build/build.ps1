40e99bedce8d6624f229cbb18da07e4e9f1e341c
Jerome Humbert <jerome.humbert@microsoft.com>
2020-05-12 20:33:58 +0100

    Revert mrwebrtc-win32 back to C++17 for now
    The NuGet pacakges for the core prebuilt libwebrtc are still based on
    C++17. Changing the project to C++14 would create a binary
    incompatibility which breaks building for CI and all users using those
    packages instead of recompiling libwebrtc from sources. So revert that
    change until there are NuGet packages or similar that are compatible
    with C++14. This does not change the Android project, which is
    unaffected and needs C++14 for binary compatibility.

--- tools/build/build.ps1
+++ tools/build/build.ps1
@@ -109,9 +109,11 @@ function Build-CoreWebRTC
     $cmd = "python ..\..\external\webrtc-uwp-sdk\scripts\run.py -a prepare build -t webrtc"
     $cmd = "$cmd -p $ScriptPlatform --cpus $BuildArch -c $BuildConfig --noColor --noWrapper"
     # UWP uses C++17 due to C++/WinRT dependency (see WebRTC UWP SDK project)
-    if ($ScriptPlatform -eq "winuwp") {
+    # FIXME - Until we have NuGet packages for libwebrtc built with C++14, do not flip the switch
+    #         and keep building with C++17 even on Win32 platform.
+    #if ($ScriptPlatform -eq "winuwp") {
         $cmd = "$cmd --cpp17"
-    }
+    #}
     Invoke-Expression $cmd | Tee-Object -Variable output
     if (Select-String -Pattern "=================== Failed" -InputObject $output -SimpleMatch -CaseSensitive -Quiet)
     {
