3862b6098329c34ef89934c09c77ba3c90992b79
Jerome Humbert <jerome.humbert@microsoft.com>
2020-06-11 17:11:26 +0100

    Re-add deleted lines

--- tools/build/libwebrtc/checkout.sh
+++ tools/build/libwebrtc/checkout.sh
@@ -9,6 +9,7 @@ set -o errexit
 set -o nounset
 # This breaks checkout on ADO, throwing some SIGPIPE for no apparent reason.
 # See http://www.pixelbeat.org/programming/sigpipe_handling.html for details.
+#set -o pipefail
 
 #-----------------------------------------------------------------------------
 function check-err() {
