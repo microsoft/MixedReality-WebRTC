5ebe549e44a3b2e9c0dc44c440ff41d3b3d9bda6
MixedReality-WebRTC BuildBot <mrwebrtc@microsoft.com>
2020-08-04 14:44:17 +0000

    Generated docs for commit 2c5b29aa1acf40fad02b561d7c79f56642f77236 (Update documentation and add UPM registry mentions (#495))

---
+++ versions/release/2.0/styles/branches.gen.js
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+// THIS FILE IS AUTO-GENERATED
+
+(function() {
+
+// List of documented branches
+var branches = [
+  'master',
+  'release/1.0',
+  'release/2.0',
+]
+
+// Export to global scope
+var mrwebrtc = {
+  branches: branches,
+  currentBranch: "release/2.0"
+}
+window.mrwebrtc = mrwebrtc
+
+})();
+
