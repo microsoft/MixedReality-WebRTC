f686614316c8e367325ea321f2b98c3a2fb72e84
rygo6 <rygo6@me.com>
2020-08-10 01:31:24 -0700

    Added ability to manually initialze unity PeerConnection

--- libs/unity/library/Runtime/Scripts/PeerConnection.cs
+++ libs/unity/library/Runtime/Scripts/PeerConnection.cs
@@ -165,6 +165,10 @@ namespace Microsoft.MixedReality.WebRTC.Unity
         [Editor.ToggleLeft]
         public bool AutoLogErrorsToUnityConsole = true;
 
+        [Tooltip("Automatically connect peer on enable.")] 
+        [Editor.ToggleLeft]
+        public bool AutoInitializeOnStart = true;
+
         #endregion
 
 
@@ -278,7 +282,7 @@ namespace Microsoft.MixedReality.WebRTC.Unity
         /// that the <see cref="Peer"/> property is non-<c>null</c> to confirm that the connection
         /// is in fact initialized.
         /// </remarks>
-        private Task InitializeAsync(CancellationToken token = default(CancellationToken))
+        public Task InitializeAsync(CancellationToken token = default(CancellationToken))
         {
             CreateNativePeerConnection();
 
@@ -576,7 +580,7 @@ namespace Microsoft.MixedReality.WebRTC.Unity
         /// <remarks>
         /// <see cref="Peer"/> will be <c>null</c> afterward.
         /// </remarks>
-        private void Uninitialize()
+        public void Uninitialize()
         {
             Debug.Assert(_nativePeer.Initialized);
             // Fire signals before doing anything else to allow listeners to clean-up,
@@ -628,7 +632,7 @@ namespace Microsoft.MixedReality.WebRTC.Unity
             {
                 OnError.AddListener(OnError_Listener);
             }
-            InitializeAsync();
+            if (AutoInitializeOnStart) InitializeAsync();
         }
 
         /// <summary>
@@ -639,7 +643,7 @@ namespace Microsoft.MixedReality.WebRTC.Unity
         /// </remarks>
         private void OnDisable()
         {
-            Uninitialize();
+            if (AutoInitializeOnStart) Uninitialize();
             OnError.RemoveListener(OnError_Listener);
         }
 
